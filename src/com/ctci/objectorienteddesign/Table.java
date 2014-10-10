941a302b95787b4e76ded760d9f976051c09cf42
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-10-09 19:28:01 -0500

    OO design

---
+++ src/com/ctci/objectorienteddesign/Table.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.ctci.objectorienteddesign;
+
+/**
+ * @author Dany
+ *
+ */
+enum TableType{
+	cuisine, royal,couples;
+}
+public class Table {
+
+
+	public int capcity;
+	
+
+}

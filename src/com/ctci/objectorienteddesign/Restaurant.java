8441062d6d83e1ee7c4e1b91662139e72bc11795
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-10-09 19:27:50 -0500

    OO design

---
+++ src/com/ctci/objectorienteddesign/Restaurant.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.ctci.objectorienteddesign;
+
+/**
+ * @author Dany
+ *
+ */
+public class Restaurant {
+
+	
+	public Host host;
+	Employee[] servers;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/ctci/objectorienteddesign/Server.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.ctci.objectorienteddesign;
+
+/**
+ * @author Dany
+ *
+ */
+public class Server {
+
+}
